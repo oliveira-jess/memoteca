4731e73544cdd599c747ef17f447d7df621130d7
Jéssica Oliveira <jessiicaoliveira12@gmail.com>
2024-09-09 23:43:25 -0300

    terminando o projeto

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,14 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
-  pensamento = {
+  @Input() pensamento:Pensamento = {
+    id: 0,
     conteudo: 'I love angular',
     autoria: 'Jess',
     modelo: 'modelo3'
-  }
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256){
+      return "pensamento-g"
+    } else {
+      return "pensamento-p"
+    }
   }
 
 }
